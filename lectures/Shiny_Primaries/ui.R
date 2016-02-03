ed6912312e57dd1e90cd8827d389ebffb0ee4f84
Navid Hassanpour <nh2519@columbia.edu>
2016-02-03 06:16:45 -0500

    Create ui.R

---
+++ lectures/Shiny_Primaries/ui.R
@@ -0,0 +1,34 @@
+```{r}
+library(shiny)
+# ui.R
+
+shinyUI(fluidPage(
+  titlePanel("Tweets per Candidate"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      helpText("Choose a specific candidate, visualize the volume of tweets"),
+      
+      selectInput("var", 
+                  label = "Choose a variable to display",
+                  choices = c("Hillary Clinton", "Bernie Sanders",
+                              "Ted Cruz", "Marco Rubio","Donald Trump"),
+                  selected = "Percent White"),
+      
+      sliderInput("range", 
+                  label = "Range of time",
+                  min = 0, max = 200, value = c(0, 200))
+    ),
+    
+    mainPanel(
+      
+      plotOutput("wordcloudT",click = "plot_click"),
+      verbatimTextOutput("info_click"),
+      plotOutput("freq_plot")
+    
+    )
+
+  )
+  
+))
+```
